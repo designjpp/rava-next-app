e57f4196512076810f46b39833e97447fc175df8
DesignJPP <designjppgv@gmail.com>
2020-10-16 18:01:25 -0300

    Update Caategory
    correção

--- src/actions/category/category.ts
+++ src/actions/category/category.ts
@@ -9,7 +9,7 @@ import { CategoryTypes } from '../types';
 export const fetchMainProductCategories = () => {
   return async (dispatch: Dispatch) => {
     try {
-      const response = await wooApi.get(
+      const response = await wooApi(
         `products/categories?hide_empty=true&parent=0`
       );
       dispatch<FetchMainProductCategories>({
@@ -25,7 +25,7 @@ export const fetchMainProductCategories = () => {
 export const fetchCategory = (id: string) => {
   return async (dispatch: Dispatch) => {
     try {
-      const response = await wooApi.get(`products/categories/${id}`);
+      const response = await wooApi(`products/categories/${id}`);
       dispatch<FetchCategory>({
         type: CategoryTypes.fetchCategory,
         payload: response.data
